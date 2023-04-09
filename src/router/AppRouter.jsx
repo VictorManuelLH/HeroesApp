e3b8a41d82662f14043d3834e34ca20ba8ea038b
VictorManuelLH <victormanuelluishernandez@gmail.com>
2023-04-08 19:02:26 -0600

    Primer commit

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,36 @@
+import { Route, Routes } from 'react-router-dom';
+
+import { HeroesRoutes } from '../heroes';
+import { LoginPages } from '../auth';
+import { PrivateRoute } from './PrivateRoute';
+import { PublicRoute } from './PublicRoute';
+
+
+
+export const AppRouter = () => {
+  return (
+    <>
+
+        <Routes>
+            
+            {/* <Route path="login" element={<LoginPages />} /> */}
+
+            <Route path='/*' element={
+              <PrivateRoute>
+                <HeroesRoutes />
+              </PrivateRoute>
+            }/>
+
+            <Route path='login' element={
+              <PublicRoute>
+                <LoginPages />
+              </PublicRoute>
+            }/>
+            
+            
+
+        </Routes>
+    
+    </>
+  )
+}
